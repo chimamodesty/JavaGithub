59ddb3e8ab38d07bbff106a7f64d25afb214a31e
chimamodesty <chimavic2000@gmail.com>
2021-10-06 21:48:46 +0100

    initial commit

---
+++ src/chinedum/Main.java
@@ -0,0 +1,10 @@
+package chinedum;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Display name on the terminal.
+	    System.out.println("Hello Chima Modesty");
+
+    }
+}
